2a30657b1673179a5819a1b05eada74c41e34c4a
Chilliope <chilliope48.com>
2023-04-17 17:18:48 +0800

    revou

--- profile.js
+++ profile.js
@@ -1,14 +1,6 @@
-const name = document.querySelector('.about-name');
-const text = name.innerHTML;
+document.addEventListener('DOMContentLoaded', function() {
+  if (document.getElementById('profile-page')) {
+    alert('Selamat datang di halaman profil kami!');
+  }
+});
 
-name.innerHTML = '';
-
-for (let i = 0; i < text.length; i++) {
-  const span = document.createElement('span');
-  span.innerHTML = text[i];
-  name.appendChild(span);
-}
-
-setTimeout(() => {
-  name.classList.add('animated');
-}, 1000);
